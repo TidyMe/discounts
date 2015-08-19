36a31f9701e66965fc9e2f2bb9e219d2fcc78c7c
pjsim <phillip.j.simmonds@gmail.com>
2015-08-19 11:39:22 +1000

    Added some helpers

--- app/helpers/discounts/coupons_helper.rb
+++ app/helpers/discounts/coupons_helper.rb
@@ -1,4 +1,15 @@
 module Discounts
   module CouponsHelper
+    def display_amount(coupon)
+      if coupon.percentage?
+        "#{coupon.amount}% off!"
+      elsif coupon.portion?
+        "#{number_to_currencey(coupon.amount)} off!"
+      end
+    end
+
+    def display_discount(coupon, price)
+      "- #{number_to_currency(coupon.discounted(price))}"
+    end
   end
 end
